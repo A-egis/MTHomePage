0e201c3d52a292c9d106d9164c0bbf18af070582
dsn <1754510323@qq.com>
2019-09-30 18:54:39 +0800

    //

--- src/axios.js
+++ src/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-axios.defaults.baseURL = 'http://open.duyiedu.com'
+axios.defaults.baseURL = '//open.duyiedu.com'
 // 拦截器 在请求或响应被 then 或 catch 处理前拦截它们。
 axios.interceptors.request.use(function (config) {
   // 在发送请求之前做些什么
