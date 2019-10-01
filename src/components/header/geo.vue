a57260533ccd9e807fcb4d1bacb6ad11a0625bf6
dsn <1754510323@qq.com>
2019-10-01 16:18:29 +0800

    修复部分样式问题

--- src/components/header/geo.vue
+++ src/components/header/geo.vue
@@ -3,9 +3,9 @@
     <div class="position">
       <i class="el-icon-location" />大连
       <router-link class="changeCity" :to="{name:'default'}">切换城市</router-link>[
-      <a href>台南</a>
-      <a href>普兰店</a>
-      <a href>瓦房店</a>]
+      <a href="#">台南</a>
+      <a href="#">普兰店</a>
+      <a href="#">瓦房店</a>]
     </div>
     <div class="m-user" v-if="!userName">
       <router-link class="login" :to="{name:'login'}">立即登录</router-link>
